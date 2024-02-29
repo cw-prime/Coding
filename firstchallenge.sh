e96c6b5da33110beb896b8a00ce2b52427ed7a72
Mark B <cw.prime@gmail.com>
2024-02-28 18:25:43 -0600

    codeing

---
+++ firstchallenge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author: Mark Barton
+# 2/27/24: 
+# Purpose: Print a string to the terminal
+
+echo "Hello My Name is"
+echo "que es su nombre"
